0669c7412e3f3a27dccc3d3ada26b47d6c766b67
unknown <Rez@.(none)>
2011-05-31 12:09:27 -0400

    Ghostframe bug fixed...

---
+++ Tukui/modules/blizzard/ghostframe.lua
@@ -0,0 +1,20 @@
+local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
+-- GhostFrame at top
+GhostFrame:SetTemplate("Transparent")
+GhostFrame:SetBackdropColor(0,0,0,0)
+GhostFrame:SetBackdropBorderColor(0,0,0,0)
+GhostFrame.SetBackdropColor = T.dummy
+GhostFrame.SetBackdropBorderColor = T.dummy
+GhostFrameContentsFrame:SetTemplate("Transparent")
+GhostFrameContentsFrame:CreateShadow()
+GhostFrameContentsFrameIcon:SetTexture(nil)
+GhostFrameContentsFrame:Width(148)
+GhostFrameContentsFrame:ClearAllPoints()
+GhostFrameContentsFrame:SetPoint("CENTER")
+GhostFrameContentsFrame.SetPoint = T.dummy
+GhostFrame:SetFrameStrata("HIGH")
+GhostFrame:SetFrameLevel(10)
+GhostFrame:ClearAllPoints()
+GhostFrame:Point("TOP", UIParent, 0, -85)
+GhostFrameContentsFrameText:ClearAllPoints()
+GhostFrameContentsFrameText:Point("BOTTOM", 0, 5)
