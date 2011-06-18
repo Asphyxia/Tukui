7e1045f7c03ee8a12961dd420f27146655df1919
unknown <Rez@.(none)>
2011-06-18 19:02:11 -0400

    changed icons size a bit...

--- Tukui_Raid/icon.lua
+++ Tukui_Raid/icon.lua
@@ -3,7 +3,7 @@ local T, C, L = unpack(Tukui) -- Import: T - functions, constants, variables; C 
 -- SWITCH LAYOUT
 if C.chat.background then
 	local swlicon = CreateFrame("Frame", "TukuiSwitchLayoutIcon", TukuiChatBackgroundRight)
-	swlicon:CreatePanel("Default", TukuiTabsRightBackground:GetHeight()+6, TukuiTabsRightBackground:GetHeight()+6, "TOPRIGHT", TukuiChatBackgroundRight, "TOPRIGHT", -3, -4)
+	swlicon:CreatePanel("Default", 20, 20, "TOPRIGHT", TukuiChatBackgroundRight, "TOPRIGHT", -3, -4)
 	swlicon:SetFrameStrata("BACKGROUND")
 	swlicon:SetFrameLevel(TukuiSwitchLayoutButton:GetFrameLevel())
 
