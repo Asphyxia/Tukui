54f0df10004678eff0d96a982a1df2bfb4707f99
Asphyxia <rez666@verizon.net>
2011-08-01 18:53:51 -0400

    Adding minimap buttons skins. (Credit Elv for the base code and Smelly for the modification).

--- Tukui/modules/panels/asphyxia_panels.lua
+++ Tukui/modules/panels/asphyxia_panels.lua
@@ -459,3 +459,43 @@ CDBars.text = T.SetFontString(CDBars, C.media.pixelfont, C["datatext"].fontsize,
 CDBars.text:SetPoint("CENTER")
 CDBars.text:SetText("Move CD Bars")
 CDBars:Hide()	
+
+-- MINIMAP BUTTONS SKINNING [Credit Elv22 for the base code and Smelly for modification.]
+local function SkinButton(f)
+    if f:GetObjectType() ~= "Button" then return end
+	f:SetPushedTexture(nil)
+    f:SetHighlightTexture(nil)
+    f:SetDisabledTexture(nil)
+	f:SetSize(22, 22)
+	
+    for i=1, f:GetNumRegions() do
+        local region = select(i, f:GetRegions())
+        if region:GetObjectType() == "Texture" then
+            local tex = region:GetTexture()
+            if tex:find("Border") or tex:find("Background") then
+                region:SetTexture(nil)
+            else
+				region:SetDrawLayer("OVERLAY", 5)
+                region:ClearAllPoints()
+                region:Point("TOPLEFT", f, "TOPLEFT", 2, -2)
+                region:Point("BOTTOMRIGHT", f, "BOTTOMRIGHT", -2, 2)
+                region:SetTexCoord(.08, .92, .08, .92)
+            end
+        end
+    end
+	f:SetTemplate("Default")
+	f:SetFrameLevel(f:GetFrameLevel() + 2)
+	
+end
+local x = CreateFrame("Frame")
+x:RegisterEvent("PLAYER_LOGIN")
+x:SetScript("OnEvent", function(self, event)
+    for i=1, Minimap:GetNumChildren() do
+        SkinButton(select(i, Minimap:GetChildren()))
+    end
+    self = nil
+end)
+
+-- move button
+MiniMapBattlefieldFrame:ClearAllPoints()
+MiniMapBattlefieldFrame:Point("BOTTOMRIGHT", TukuiMinimap, "BOTTOMRIGHT", -4, 4)
