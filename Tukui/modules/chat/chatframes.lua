d3f14bc3ef7162deac7a7cf2f60c5025e3a2b01e
Asphyxia <rez666@verizon.net>
2011-09-13 21:55:44 -0400

    Battlenet toast frame position fixed.

--- Tukui/modules/chat/chatframes.lua
+++ Tukui/modules/chat/chatframes.lua
@@ -70,7 +70,7 @@ local function StyleChat(frame)
 	end
 	tabtext:SetShadowOffset(0, 0)
 	tabtext:ClearAllPoints()
-	tabtext:Point("CENTER", tab, "CENTER", 0, -3)
+	tabtext:Point("CENTER", tab, "CENTER", 0, -2)
 	
 	--_G[chat]:SetFont(C.media.pixelfont, C["datatext"].fontsize +1, "MONOCHROMEOUTLINE")
 	_G[chat]:SetClampRectInsets(0,0,0,0)
@@ -190,7 +190,7 @@ hooksecurefunc("FCF_UnDockFrame", ChatPosition)
 local function ToastFramePosition(self)
 	BNToastFrame:HookScript("OnShow", function(self)
 		self:ClearAllPoints()
-		self:Point("BOTTOMLEFT", TukuiChatLeft, "TOPLEFT", 0, 3)
+		self:Point("BOTTOMLEFT", TukuiChatBackgroundLeft, "TOPLEFT", 0, 3)
 	end)
 end
 
